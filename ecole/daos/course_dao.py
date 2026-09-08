a9077bca2a3740bff30dcf28076bc3719c6e1788
MathieuROCANCOURT <mathieu.rocancourt@fms-ea.com>
2026-09-08 09:45:12 +0200

    chore: Init project with all DAO complete without teacher_dao.

---
+++ ecole/daos/course_dao.py
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+
+"""
+Classe Dao[Course]
+"""
+from models.course import Course
+from daos.dao import Dao
+from dataclasses import dataclass
+from typing import Optional
+
+from models.student import Student
+from models.teacher import Teacher
+
+
+@dataclass
+class CourseDao(Dao[Course]):
+    def create(self, course: Course) -> int:
+        """Crée en BD l'entité Course correspondant au cours course
+
+        :param course: à créer sous forme d'entité Course en BD
+        :return: l'id de l'entité insérée en BD (0 si la création a échoué)
+        """
+        with Dao.connection.cursor() as cursor:
+            sql = """
+                    INSERT INTO course(name, start_date, end_date, id_teacher)
+                    VALUES (%s,%s,%s,%s);
+                """
+            cursor.execute(sql, (course.name, course.start_date, course.end_date, course.teacher.id))
+            return cursor.lastrowid
+
+    def read(self, id_course: int) -> Optional[Course]:
+        """Renvoit le cours correspondant à l'entité dont l'id est id_course
+           (ou None s'il n'a pu être trouvé)"""
+        course: Optional[Course]
+
+        with Dao.connection.cursor() as cursor:
+            sql = """
+                SELECT * FROM course
+                JOIN teacher ON course.id_teacher = teacher.id_teacher
+                JOIN person ON person.id_person = teacher.id_person
+                WHERE course.id_course=%s;
+                """
+            cursor.execute(sql, (id_course,))
+            record = cursor.fetchone()
+        if record is not None:
+            course = Course(record["name"],
+                            record["start_date"],
+                            record["end_date"])
+            course.id = record["id_course"]
+
+            course.set_teacher(Teacher(record["first_name"],
+                                       record["last_name"],
+                                       record["age"],
+                                       record["hiring_date"]))
+            with Dao.connection.cursor() as cursor:
+                sql = """
+                    SELECT first_name, last_name, age FROM course
+                    JOIN takes ON takes.id_course = course.id_course
+                    JOIN student ON student.student_nbr = takes.student_nbr
+                    JOIN person ON person.id_person = student.id_person
+                    WHERE course.id_course=%s;
+                    """
+                cursor.execute(sql, (id_course,))
+                record = cursor.fetchall()
+
+                for student_coord in record:
+                    course.add_student(
+                        Student(
+                            student_coord["first_name"],
+                            student_coord["last_name"],
+                            student_coord["age"]
+                        )
+                    )
+
+        else:
+            course = None
+
+        return course
+
+    def update(self, course: Course) -> bool:
+        """Met à jour en BD l'entité Course correspondant à course, pour y correspondre
+
+        :param course: cours déjà mis à jour en mémoire
+        :return: True si la mise à jour a pu être réalisée
+        """
+        with Dao.connection.cursor() as cursor:
+            sql = """
+                    UPDATE course
+                    SET name=%s start_date=%s end_date=%s id_teacher=%s
+                    WHERE id_course=%s;
+                """
+            cursor.execute(sql, (course.name, course.start_date, course.end_date, course.teacher.id, course.id))
+            return cursor.rowcount > 0
+
+    def delete(self, course: Course) -> bool:
+        """Supprime en BD l'entité Course correspondant à course
+
+        :param course: cours dont l'entité Course correspondante est à supprimer
+        :return: True si la suppression a pu être réalisée
+        """
+        with Dao.connection.cursor() as cursor:
+            sql = """
+                    DELETE FROM course
+                    WHERE id_course=%s;
+                """
+            cursor.execute(sql, (course.id,))
+            return cursor.rowcount > 0
---
+++ ecole/daos/student_dao.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+
+"""
+Classe Dao[Student]
+"""
+from daos import course_dao, address_dao
+from models.address import Address
+from models.student import Student
+from daos.dao import Dao
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class StudentDao(Dao[Student]):
+    def create(self, student: Student) -> int:
+        """Crée en BD l'entité Student correspondant au cours student
+
+        :param student: à créer sous forme d'entité Student en BD
+        :return: l'id de l'entité insérée en BD (0 si la création a échoué)
+        """
+        with Dao.connection.cursor() as cursor:
+            sql = """
+                    INSERT INTO person(first_name, last_name, age, id_address)
+                    VALUES (%s,%s,%s,%s);
+                """
+            cursor.execute(sql, (student.first_name, student.last_name, student.age, student.address.id))
+            record = cursor.fetchone()
+
+            sql = """
+                    INSERT INTO student(student_nbr, id_person)
+                    VALUES (%s,%s);
+                   """
+            cursor.execute(sql, (student.student_nbr, record.index("id_person")))
+
+            for course in student.courses_taken:
+                sql = "INSERT INTO takes(student_nbr, id_course) VALUES (%s,%s);"
+                cursor.execute(sql, (student.student_nbr, course))
+
+            return cursor.lastrowid
+
+    def read(self, student_nbr: int) -> Optional[Student]:
+        """Renvoit l'étudiant correspondant à l'entité dont l'id est student_nbr
+           (ou None s'il n'a pu être trouvé)"""
+        student: Optional[Student]
+
+        with Dao.connection.cursor() as cursor:
+            sql = """
+                    SELECT *
+                    FROM student s
+                    JOIN person p ON s.id_person = p.id_person
+                    WHERE s.student_nbr = %s;
+                """
+
+            cursor.execute(sql, (student_nbr,))
+            record = cursor.fetchone()
+
+        if record is None:
+            return None
+
+        student = Student(
+            record["first_name"],
+            record["last_name"],
+            record["age"]
+        )
+
+        student.student_nbr = record["student_nbr"]
+
+        student.address = address_dao.AddressDao().read(
+            record["id_address"]
+        )
+
+        return student
+
+    def update(self, student: Student) -> bool:
+        """Met à jour en BD l'entité Student correspondant à student, pour y correspondre
+
+        :param student: cours déjà mis à jour en mémoire
+        :return: True si la mise à jour a pu être réalisée
+        """
+        with Dao.connection.cursor() as cursor:
+            return cursor.rowcount > 0
+
+    def delete(self, student: Student) -> bool:
+        """Supprime en BD l'entité Student correspondant à student
+
+        :param student: cours dont l'entité Student correspondante est à supprimer
+        :return: True si la suppression a pu être réalisée
+        """
+        with Dao.connection.cursor() as cursor:
+            return cursor.rowcount > 0
